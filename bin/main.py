d107f03d23c6a08f90fa39059508a1d380062274
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-07-29 19:17:53 -0400

    handling clock better

--- bin/main.py
+++ bin/main.py
@@ -14,16 +14,21 @@ def _main() -> None:
     # start the clock
     clock = Clock()
     
-    # parse command line arguments and build the Config object
-    config = Config(__version__, __author__)  
+    try:
+        # parse command line arguments and build the Config object
+        config = Config(__version__, __author__)  
+        
+        # only do work if help wasn't requested
+        if not config.helpRequested:
+            # get the core loci allele calls
+            core = _parseAlleleCalls(config)
+            
+            # create the multiple sequence alignment
+            _createMsa(config, core)
+            
+            # print the status
+            print(f'\ntotal runtime: {clock.getTimeString()}')
     
-    # only do work if help wasn't requested
-    if not config.helpRequested:
-        # get the core loci allele calls
-        core = _parseAlleleCalls(config)
-        
-        # create the multiple sequence alignment
-        _createMsa(config, core)
-        
-        # print the status
-        print(f'\ntotal runtime: {clock.getTimeString()}')
+    except Exception as e:
+        Clock._killWheel()
+        raise Exception(e)

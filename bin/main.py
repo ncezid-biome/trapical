f6b144c34894d1cb2dee9c675c127ab5cc2c8472
dr-joe-wirth <dr.joe.wirth@gmail.com>
2024-07-29 18:30:59 -0400

    better documentation

--- bin/main.py
+++ bin/main.py
@@ -3,17 +3,27 @@ from bin.Config import Config
 from bin.createMsa import _createMsa
 from bin.parseAlleleCalls import _parseAlleleCalls
 
+
 __author__ = "Joseph S. Wirth"
 __version__ = "0.0.1"
 
 
 def _main() -> None:
+    """main runner function
+    """
+    # start the clock
     clock = Clock()
     
-    config = Config(__version__)
+    # parse command line arguments and build the Config object
+    config = Config(__version__, __author__)  
     
-    seqs,core = _parseAlleleCalls(config)
-    
-    _createMsa(config, seqs, core)
-    
-    print(f'\ntotal runtime: {clock.getTimeString()}')
+    # only do work if help wasn't requested
+    if not config.helpRequested:
+        # get the core loci allele calls
+        core = _parseAlleleCalls(config)
+        
+        # create the multiple sequence alignment
+        _createMsa(config, core)
+        
+        # print the status
+        print(f'\ntotal runtime: {clock.getTimeString()}')
